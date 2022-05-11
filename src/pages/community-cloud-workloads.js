92b19a711e110f9f0419392f048c0ec88e3905a7
Marcel Hild <hild@b4mad.net>
2022-05-11 18:33:00 +0200

    lint

--- src/pages/community-cloud-workloads.js
+++ src/pages/community-cloud-workloads.js
@@ -13,11 +13,11 @@ const CommunityCloudPage = () => {
         <Container>
         <Title order={2} my="md">Op1st Community Cloud</Title>
 
-        <iframe src="https://grafana-public.operate-first.cloud/d-solo/opf-overview/workload-overview?orgId=1&var-datasource=default&var-cluster=moc/smaug&var-namespace=All&theme=light&panelId=4" width="1200" height="400" frameborder="0"></iframe>
+        <iframe src="https://grafana-public.operate-first.cloud/d-solo/opf-overview/workload-overview?orgId=1&var-datasource=default&var-cluster=moc/smaug&var-namespace=All&theme=light&panelId=4" width="1200" height="400" frameBorder="0"></iframe>
 
-        <iframe src="https://grafana-public.operate-first.cloud/d-solo/opf-overview/workload-overview?orgId=1&var-datasource=default&var-cluster=moc/infra&var-namespace=All&theme=light&panelId=4" width="1200" height="400" frameborder="0"></iframe>
+        <iframe src="https://grafana-public.operate-first.cloud/d-solo/opf-overview/workload-overview?orgId=1&var-datasource=default&var-cluster=moc/infra&var-namespace=All&theme=light&panelId=4" width="1200" height="400" frameBorder="0"></iframe>
 
-        <iframe src="https://grafana-public.operate-first.cloud/d-solo/opf-overview/workload-overview?orgId=1&var-datasource=default&var-cluster=emea/balrog&var-namespace=All&theme=light&panelId=4" width="1200" height="400" frameborder="0"></iframe>
+        <iframe src="https://grafana-public.operate-first.cloud/d-solo/opf-overview/workload-overview?orgId=1&var-datasource=default&var-cluster=emea/balrog&var-namespace=All&theme=light&panelId=4" width="1200" height="400" frameBorder="0"></iframe>
         </Container>
         <Footer/>
         </main>
