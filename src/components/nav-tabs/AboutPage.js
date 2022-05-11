446dcc92a5e208c2704510c47afae3f4dcae8d66
Stephanie E. Watson <swatson@redhat.com>
2022-05-11 19:50:43 -0400

    Fixed repeated text on homepage; added to the Our Purpose and Docs and Training pages (#26)

--- src/components/nav-tabs/AboutPage.js
+++ src/components/nav-tabs/AboutPage.js
@@ -6,12 +6,48 @@ export function AboutContent() {
         <Container>
             <Title order={2} my="md">Our Purpose</Title>
             <Text>
-While open source has made software widely available, it has also exposed another barrier to entry: the ability to operate software in production. Proprietary services for operations undermine the open source model, so we must now shift our focus to open sourcing operations.
+              Open source software is widely available, but it faces an
+              operatations barrier to entry into production environments.
+              Proprietary services have attempted to address thie barrier, but
+              they undermine the open source model.
             </Text>
             <Text>
-In doing so, developers and operators collaborate to apply a project’s operational considerations right back into the code. The result?
+              To overcome this operations barrier in an open source way, we
+              must which so an <b><i>open source approach to operations</i></b>.
+              This means developers and operators collaborate to apply a
+              product&apos;s operational considerations right back into the code
+              itself.
+            </Text>
+            <Text>
+              The result?
             </Text>
             <Title order={4} my="md">Better code, better performance, better final product.</Title>
+            <Text>
+              <b>Operate First</b> is a community of developers, operators,
+              and others who believe in promoting this open source approach
+              to operations. We invite open source developers to bring their
+              projects to a production cloud during development hosted and
+              maintained through Operate First contributors. There, they
+              can interact with ther cloud provider&apos;s operations (people and
+              tools) and gain valuable feedback on operations considerations
+              for their projects.
+            </Text>
+            <Title order={4} my="md">Where to start</Title>
+            <Text>
+              Continue learning more about Operate First: <a href="https://www.operate-first.cloud/training/operate_first_intro" target="_blank" rel="noreferrer">Introduction to Operate First</a>
+            </Text>
+            <Text>
+              <a href="http://localhost:8000/community">Get involved in our community</a>
+            </Text>
+            <Text>
+              Start using Operate First cloud resources as an app developer (link coming soon)
+            </Text>
+            <Text>
+              Start contributing to Operate First as an operator or SRE (link coming soon)
+            </Text>
+            <Text>
+              See how data scientists are using Operate First (link coming soon)
+            </Text>
         </Container>
     )
 }
