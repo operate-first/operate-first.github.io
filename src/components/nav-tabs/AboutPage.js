0df7b9d8c02ed8efb88dd501bcebad11822f35c7
Thor*sten Schwesig <89909507+schwesig@users.noreply.github.com>
2022-05-11 04:13:39 -0400

    update About & Community

--- src/components/nav-tabs/AboutPage.js
+++ src/components/nav-tabs/AboutPage.js
@@ -4,8 +4,14 @@ import React from 'react';
 export function AboutContent() {
     return (
         <Container>
-            <Title order={2} my="md">Op1st Community Cloud</Title>
-            <Text>Content here to be finalized</Text>
+            <Title order={2} my="md">Our Purpose</Title>
+            <Text>
+While open source has made software widely available, it has also exposed another barrier to entry: the ability to operate software in production. Proprietary services for operations undermine the open source model, so we must now shift our focus to open sourcing operations.
+            </Text>
+            <Text>
+In doing so, developers and operators collaborate to apply a project’s operational considerations right back into the code. The result?
+            </Text>
+            <Title order={3} my="md">Better code, better performance, better final product.</Title>
         </Container>
     )
 }
--- src/components/nav-tabs/CommunityPage.js
+++ src/components/nav-tabs/CommunityPage.js
@@ -1,31 +1,99 @@
 import { Button, Container, Text, Title, Group } from '@mantine/core';
 import React from 'react';
 
+import { createStyles, SimpleGrid, ThemeIcon, Grid, Col } from '@mantine/core';
+import { BuildingCommunity, MailFast, Speakerphone, BrandTwitter } from 'tabler-icons-react';
+
+const useStyles = createStyles((theme) => ({
+    wrapper: {
+      padding: `${theme.spacing.xl * 2}px ${theme.spacing.xl}px`,
+    },
+
+    title: {
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        fontSize: 36,
+        fontWeight: 900,
+        lineHeight: 1.1,
+        marginBottom: theme.spacing.md,
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+}));
+
+const features = [
+    {
+        icon: BuildingCommunity,
+        title: 'Community mailing list',
+        description: 'for Operate First project user and contributor discussions',
+    },
+    {
+        icon: Speakerphone,
+        title: 'Announcements list',
+        description: 'for the latest project news',
+    },
+    {
+        icon: MailFast,
+        title: 'OpenInfra Labs mailing list',
+        description:
+        'for those interested in open source cloud operations more broadly',
+    },
+    {
+        icon: BrandTwitter,
+        title: 'Twitter',
+        description:
+        'You can also find us on Twitter, or view community project demos and updates on our YouTube',
+    },
+];
+
 export function CommunityContent() {
+    const { classes } = useStyles();
+
+    const items = features.map((feature) => (
+        <div key={feature.title}>
+        <ThemeIcon
+            size={44}
+            radius="md"
+            variant="gradient"
+            gradient={{ deg: 133, from: 'blue', to: 'cyan' }}
+        >
+            <feature.icon size={26} />
+        </ThemeIcon>
+        <Text size="lg" mt="sm" weight={500}>
+            {feature.title}
+        </Text>
+        <Text color="dimmed" size="sm">
+            {feature.description}
+        </Text>
+        </div>
+    ));
+
     return (
-        <Container>
-            <Title order={2} my="md">Get involved in Operate First</Title>
-            <Text>
-                The Operate First community makes use of the GitHub open source DevOps platform and integrated community management tools. The links here direct you to resources within that community space at GitHub:
-            </Text>
-            <Title order={2} my="md">About the community</Title>
-            <Text>Code of Conduct</Text>
-            <Text>Community Handbook</Text>
-            <Text>Special Interest Groups</Text>
-            <Text>Participating Projects</Text>
-            <Title order={2} my="md">Getting involved</Title>
-            <Text>Mailing List</Text>
-            <Text>Slack team chat</Text>
-            <Title order={2} my="md">Operate First Blog</Title>
-            <Text>For complete details, read the <a href="#">Operate First Community Manifesto</a></Text>
-            <Title order={2} my="md">Community Resources</Title>
-            <Group style={{ marginBottom: 50 }}>
-                <Button color="dark">Our Environment</Button>
-                <a href="https://www.operate-first.cloud/community/README.html">
-                    <Button color="dark">Open community cloud</Button>
-                </a>
-                <Button color="dark">SRE Community of Practice</Button>
-            </Group>
-        </Container>
-    )
+        <div className={classes.wrapper}>
+            <Grid gutter={80}>
+            <Col span={12} md={5}>
+                <Title className={classes.title} order={2}>
+                Who are we?
+                </Title>
+                <Text color="dimmed">
+                We are data scientists, software engineers and DevOps professionals working within the Operate First framework on open source software with open infrastructure, focused on solving large scale operational issues around developing intelligent applications and managing hybrid cloud systems through the application of advanced automation and machine learning.
+                </Text>
+
+                <Text color="dimmed">
+                Join us!
+                </Text>
+                <Text color="dimmed">
+                    Find our code on GitHub, meet the community on Slack, and join our mailing lists for announcements and discussions:
+                </Text>
+
+                <Text color="dimmed">
+                    If you’re interested in data science, check out our data science projects. If you are more interested in site reliability engineering, we have SRE resources to help you get started!
+                </Text>
+            </Col>
+            <Col span={12} md={7}>
+                <SimpleGrid cols={2} spacing={30} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+                {items}
+                </SimpleGrid>
+            </Col>
+            </Grid>
+        </div>
+    );
 }
