3063a59c776ff3353ac0a710754221ecec865f07
Stephanie E. Watson <swatson@redhat.com>
2022-05-12 10:36:48 -0400

    Repaired typos and spacing in the Our Purpose page (#32)

--- src/components/nav-tabs/AboutPage.js
+++ src/components/nav-tabs/AboutPage.js
@@ -5,47 +5,47 @@ export function AboutContent() {
     return (
         <Container style={{ paddingBottom: 574 }}>
             <Title order={2} my="md">Our Purpose</Title>
-            <Text>
+            <Text pb="sm">
               Open source software is widely available, but it faces an
               operatations barrier to entry into production environments.
-              Proprietary services have attempted to address thie barrier, but
+              Proprietary services have attempted to address this barrier, but
               they undermine the open source model.
             </Text>
-            <Text>
+            <Text pb="sm">
               To overcome this operations barrier in an open source way, we
-              must which so an <b><i>open source approach to operations</i></b>.
-              This means developers and operators collaborate to apply a
+              must switch to an <b><i>open source approach to operations</i></b>.
+              This means that developers and operators collaborate to apply a
               product&apos;s operational considerations right back into the code
               itself.
             </Text>
             <Text>
-              The result?
+              <i>The result?</i>
             </Text>
-            <Title order={4} my="md">Better code, better performance, better final product.</Title>
-            <Text>
+            <Title order={4} my="md" py="sm">Better code, better performance, better final product.</Title>
+            <Text pb="sm">
               <b>Operate First</b> is a community of developers, operators,
               and others who believe in promoting this open source approach
               to operations. We invite open source developers to bring their
               projects to a production cloud during development hosted and
               maintained through Operate First contributors. There, they
-              can interact with ther cloud provider&apos;s operations (people and
-              tools) and gain valuable feedback on operations considerations
+              can interact with ther cloud provider&apos;s operations (people
+              and tools) and gain valuable feedback on operations considerations
               for their projects.
             </Text>
-            <Title order={4} my="md">Where to start</Title>
+            <Title order={4} my="md">Where to start:</Title>
             <Text>
               Continue learning more about Operate First: <a href="https://www.operate-first.cloud/training/operate_first_intro" target="_blank" rel="noreferrer">Introduction to Operate First</a>
             </Text>
-            <Text>
-              <a href="http://localhost:8000/community">Get involved in our community</a>
+            <Text pt={10}>
+              <a href="community">Get involved in our community</a>
             </Text>
-            <Text>
+            <Text pt={10}>
               Start using Operate First cloud resources as an app developer (link coming soon)
             </Text>
-            <Text>
+            <Text pt={10}>
               Start contributing to Operate First as an operator or SRE (link coming soon)
             </Text>
-            <Text>
+            <Text pt={10}>
               See how data scientists are using Operate First (link coming soon)
             </Text>
         </Container>
