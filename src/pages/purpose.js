932c29f43ee8d4cdd60e595da68cf36d921faa57
Marcel Hild <hild@b4mad.net>
2022-06-23 18:17:11 -0500

    add layout for about page

--- src/pages/purpose.js
+++ src/pages/purpose.js
@@ -1,17 +1,16 @@
 import * as React from "react";
 import '../pages/style.css';
 // Component imports
-import { Nav } from "../components/homepage/Navbar";
-import { Footer } from "../components/homepage/Footer";
 import { AboutContent } from "../components/nav-tabs/AboutPage";
+import { Layout } from "../components/Layout";
 
 const PurposePage = () => {
 
   return (
     <main>
-      <Nav />
-      <AboutContent />
-      <Footer />
+      <Layout>
+        <AboutContent />
+      </Layout>
     </main>
   )
 }
