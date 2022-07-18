aca93818445e41049974fef636d85ef414d38e3e
Mike Knell <m@blat.at>
2022-07-18 16:21:21 +0200

    Remove a broken link (the community handbook isn't there yet), and tidy text for readability

--- src/pages/srepractices.js
+++ src/pages/srepractices.js
@@ -13,18 +13,15 @@ const SREPracticesPage = () => {
       <Container pb={455}>
         <Title order={2} my="md">SRE Practices</Title>
         <Text>
-          Beginning in January 2022 the Op1st community is releasing early (and often) a growing set of course materials for learning SRE methods and practices. These courses will be tied into the running community cloud, providing a look behind the normal secrecy wall around operations.
+          Beginning soon the Op1st community plans to release early (and often) a growing set of course materials for learning SRE methods and practices. These courses will be tied into the running community cloud, providing a look behind the wall of secrecy that normally surrounds the world of operations.
         </Text>
         <Text>
-          Our plans for these courses is extensive and will be laid out on these pages. In the meantime, DIY learners are encouraged to begin looking around at how project operations are run, as a view into how the training courses are going to look.
+          Our plan for these courses will be laid out on these pages. In the meantime, DIY learners are encouraged to begin looking around at how project operations are run to get an idea of how the training courses are going to look.
         </Text>
         <Title order={3} py="md">
           Additional Resources
         </Title>
         <List>
-          <List.Item>
-            <a href="https://www.operate-first.cloud/community-handbook/operations-intro.md">SRE pages in the Op1st Community Handbook</a> used by the Op1st Operations SIG.
-          </List.Item>
           <List.Item py="xs">
             The <a href="https://www.youtube.com/channel/UCe87bwqlGoBQs2RvMQZ5_sg/playlists">Op1st video playlists</a> cover various aspects of how the community cloud works, and other aspects of the community.
           </List.Item>
