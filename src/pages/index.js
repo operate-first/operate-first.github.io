2d0be1f375caddae8de9c9c9a24d8ff9dfd92108
Marcel Hild <hild@b4mad.net>
2022-07-20 12:50:49 +0200

    add layout to front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,21 @@
 import * as React from "react";
 import '../pages/style.css';
 // Component imports
-import { HomeContent } from "../components/homepage/HomePage";
+import { HomePage } from "../components/homepage/HomePage";
 import { AboutFeatures } from "../components/homepage/About";
 import { InterestedField } from "../components/homepage/Interest";
 import { FaqSimple } from "../components/homepage/Faq";
-import { Nav } from "../components/homepage/Navbar";
-import { Footer } from "../components/homepage/Footer";
+import { Layout } from "../components/Layout";
 
 const IndexPage = () => {
 
   return (
-    <main>
-      <Nav />
-      <HomeContent />
+    <Layout>
+      <HomePage />
       <AboutFeatures />
       <InterestedField />
       <FaqSimple />
-      <Footer />
-    </main>
+    </Layout>
   )
 }
 
