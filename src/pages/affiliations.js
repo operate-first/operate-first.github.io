15e0ed04a18602704e4dd6a154042dff0a42bd57
Thor*sten Schwesig <89909507+schwesig@users.noreply.github.com>
2022-08-23 13:42:05 -0400

    v3 RH logo to the right, add affiliates link dummy MOC Alliance & OpenInfra Labs Logo & Link white logos in footer

---
+++ src/pages/affiliations.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Helmet from 'react-helmet';
+import { Container, Title, Button, Text } from '@mantine/core';
+import { Cloud } from 'tabler-icons-react';
+import { Layout } from '../components/Layout';
+
+const ActionPage = () => {
+  return (
+    <Layout>
+      <Container>
+        <Title order={2} my="md">
+          Affiliations
+        </Title>
+        <Text> tbd </Text>
+      </Container>
+    </Layout>
+  );
+};
+
+export default ActionPage;
