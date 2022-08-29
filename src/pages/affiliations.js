2e1385382326dd2f8db1d1997c375be225579b47
Thor*sten Schwesig <89909507+schwesig@users.noreply.github.com>
2022-08-29 15:00:33 -0400

    add affiliations page and logos

--- src/pages/affiliations.js
+++ src/pages/affiliations.js
@@ -1,8 +1,14 @@
 import * as React from 'react';
-import Helmet from 'react-helmet';
-import { Container, Title, Button, Text } from '@mantine/core';
-import { Cloud } from 'tabler-icons-react';
+import { Container, Title } from '@mantine/core';
 import { Layout } from '../components/Layout';
+
+import { Grid, Skeleton } from '@mantine/core';
+import { Image } from '@mantine/core';
+import mocalliancelogo from '../assets/Aff-Logo-MOC-Alliance.png';
+import openinfralabslogo from '../assets/Aff-Logo-OpenInfraLabs.png';
+import rhlogo from '../assets/Aff-Logo-Red-Hat.png';
+
+const child = <Skeleton height={140} radius="md" animate={false} />;
 
 const ActionPage = () => {
   return (
@@ -11,7 +17,77 @@ const ActionPage = () => {
         <Title order={2} my="md">
           Affiliations
         </Title>
-        <Text> tbd </Text>
+        <Container my="md">
+          <Grid>
+            <Grid.Col xs={4}>
+              <a
+                href="https://massopen.cloud/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  style={{
+                    width: 200,
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    marginTop: 'auto',
+                    marginBottom: 'auto',
+                  }}
+                >
+                  <Image radius="md" src={mocalliancelogo} alt="MOC Alliance" />
+                </div>
+              </a>
+            </Grid.Col>
+            <Grid.Col xs={8}>
+              Mission The MOC Alliance aims to accelerate the pace of discovery
+              and innovation in an open production research-focused cloud.
+            </Grid.Col>
+            <Grid.Col xs={8}>{child}</Grid.Col>
+            <Grid.Col xs={4}>
+              <a
+                href="https://openinfralabs.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  style={{
+                    width: 200,
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    marginTop: 'auto',
+                    marginBottom: 'auto',
+                  }}
+                >
+                  <Image
+                    radius="md"
+                    src={openinfralabslogo}
+                    alt="OpenInfra Labs"
+                  />
+                </div>
+              </a>
+            </Grid.Col>
+            <Grid.Col xs={4}>
+              <a
+                href="https://www.redhat.com/en"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  style={{
+                    width: 200,
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    marginTop: 'auto',
+                    marginBottom: 'auto',
+                  }}
+                >
+                  <Image radius="md" src={rhlogo} alt="Red Hat" />
+                </div>
+              </a>
+            </Grid.Col>
+            <Grid.Col xs={8}>{child}</Grid.Col>
+          </Grid>
+        </Container>
       </Container>
     </Layout>
   );
