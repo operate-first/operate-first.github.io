c47822b59333f4b3db1f43dd37569e9630e8aec9
Thor*sten Schwesig <89909507+schwesig@users.noreply.github.com>
2022-10-24 18:51:24 -0400

    v2 adding 1 redirect jscript to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { Link } from 'gatsby';
 import {
   createStyles,
@@ -65,6 +66,13 @@ export default function NotFoundPage() {
       >
         Unfortunately, this is only a 404 page. You may have mistyped the
         address, or the page has been moved to another URL :(
+        <Helmet>
+          <script>
+            {
+              'const urlRegex = "/community-handbook/*";if (window.location.pathname.match(urlRegex) !== null){window.location.replace("https://old.operate-first.cloud/community-handbook/");}'
+            }
+          </script>
+        </Helmet>
       </Text>
       <Group position="center">
         <Link to="/">
